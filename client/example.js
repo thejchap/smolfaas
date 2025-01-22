bef9704dda0803aba726fe6195a14f87373864f3
Justin Chapman <commonmodestudio@gmail.com>
2025-01-22 18:35:33 -0500

    checkpoint 2025-01-22T18:35:33-0500

--- client/example.js
+++ client/example.js
@@ -3,6 +3,4 @@ import { run } from ".";
 function hello(world, exclamation) {
     return `Hello, ${world}${exclamation}`;
 }
-const result = await run(hello, "world", 3);
-
-console.log(result);
+console.log(await run(hello, "world", 3));
