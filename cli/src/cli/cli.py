1c6c8a64b51562a883d10c5e4a66c3a8d316aa60
Justin Chapman <commonmodestudio@gmail.com>
2025-01-30 21:10:45 -0500

    checkpoint 2025-01-30T21:10:45-0500

--- cli/src/cli/cli.py
+++ cli/src/cli/cli.py
@@ -9,7 +9,7 @@ from rich import print
 CLI = typer.Typer()
 FUNCTIONS_CLI = typer.Typer()
 CLI.add_typer(FUNCTIONS_CLI, name="functions")
-DEFAULT_BASE_URL = "https://tinyfaas.fly.dev"
+DEFAULT_BASE_URL = "https://tinyfaas.jchap.me"
 
 
 Source = Annotated[
